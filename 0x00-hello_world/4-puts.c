2d9f795f0f62ee91909b5fd602e07060e2c619f4
KhanyisaMgobo <khanyisamgobo392@gmail.com>
2022-10-13 13:24:41 -0700

    Task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main- Serves as the entry point  for the program
  * Return: By default return zero if no errors otherwise return non-zero value.
  */
-int main(void)
+int main (void)
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts ("\" Programming is like building a multilingual puzzle");
 	return (0);
 }
 
