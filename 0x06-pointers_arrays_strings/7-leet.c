20de111e6a20ea0770c447a05f0a06dd17a2d569
KhanyisaMgobo <khanyisamgobo392@gmail.com>
2022-10-26 17:11:11 -0700

    Task_7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: input
+ * Return: the values of n
+ */
+char *leet(char *n)
+{
+	int k, m;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (k = 0; n[k] != '\0'; k++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			if (n[k] == s1[m])
+			{
+				n[k] = s2[m];
+			}
+		}
+	}
+	return (n);
+}
